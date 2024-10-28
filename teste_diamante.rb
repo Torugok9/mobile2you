71d003689c41a6bf30a63bd8fa130cddc4523f59
Victor Hugo <85053053+Torugok9@users.noreply.github.com>
2024-10-28 18:10:41 -0300

    Update teste_diamante.rb

--- teste_diamante.rb
+++ teste_diamante.rb
@@ -1,10 +1,10 @@
 # Expressão
 expressao = "<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>"
 
-# Passo 1: Contar a quantidade de areias
+# Contar a quantidade de areias
 quantidade_areias = expressao.count(".")
 
-# Passo 2: Contar e remover os diamantes
+# Contar e remover os diamantes
 def contar_diamantes(expressao)
   diamantes = 0
   pilha = 0
@@ -21,6 +21,6 @@ def contar_diamantes(expressao)
   diamantes
 end
 
-# Passo 3: Exibir a quantidade de diamantes extraídos
+# Exibir a quantidade de diamantes extraídos
 puts "Quantidade de diamantes extraídos: #{quantidade_areias}"
 puts "Quantidade de areias extraídas: #{contar_diamantes(expressao)}"
